53b54b6bd10a678b763fd701b58062b869adcf11
EC2 Default User <ec2-user@ip-172-31-34-147.us-east-2.compute.internal>
2018-08-08 06:16:05 +0000

    first commit

---
+++ shopping.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+    {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+    }    
+  );
+});
+
+  $(function(){
+    $(document).ready(function(){
+
+      $("#sp_navi_btn").click(function () {
+        $(".sp_navi_li").slideToggle();
+      });
+
+    });
+  });
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
